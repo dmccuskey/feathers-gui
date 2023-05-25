e546ad7e0f822166b187297893fe98d70ce146d9
David Mc <my-gitkraken@munkie.biz>
2023-05-25 07:53:12 -0600

    remove public path, add comment about CORS

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
 const port = 9092 // Easy to change the port here
 
 module.exports = {
-  publicPath: `http://localhost:${port}/`,
   devServer: {
     port, // This will take care of desktop machine
     host: `0.0.0.0`, // This will take care of mobile device
     headers: {
+      // added CORS for localhost or IP access
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Headers':
         'Origin, X-Requested-With, Content-Type, Accept',
